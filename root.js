4bcedc701e3a73c467247a11491472a325314e07
Sabituddin Bigbang <sabituddin@inception.asia>
2022-07-28 19:39:14 +0700

    add language

--- root.js
+++ root.js
@@ -1,197 +1,17 @@
-const dataCoding = [
-  {
-    icon: "fa-brands fa-html5",
-    title: "HTML",
-    color: "text-orange-500",
-  },
-  {
-    icon: "fa-brands fa-css3",
-    title: "CSS 3",
-    color: "text-cyan-500",
-  },
-  {
-    icon: "fa-brands fa-js",
-    title: "Javascript",
-    color: "text-yellow-500",
-  },
-  {
-    icon: "fa-solid fa-c",
-    title: "C/C++",
-    color: "text-yellow-500",
-  },
-  {
-    icon: "fa-brands fa-python",
-    title: "Python",
-    color: "text-sky-500",
-  },
-  {
-    icon: "fa-brands fa-java",
-    title: "Java",
-    color: "text-red-500",
-  },
-  {
-    icon: "fa-solid fa-code",
-    title: "Dart",
-    color: "text-blue-500",
-    url: "https://avatars.githubusercontent.com/u/1609975?s=200&v=4",
-  },
-  {
-    icon: "fa-brands fa-golang",
-    title: "GoLang",
-    color: "text-cyan-500",
-  },
-  {
-    icon: "fa-brands fa-kotlin",
-    title: "Kotlin",
-    color: "text-Indigo-500",
-    url: "https://abrudz.github.io/logos/Kotlin.svg",
-  },
-  {
-    icon: " ",
-    title: "Free Pascal",
-    color: "text-Indigo-500",
-    url: "https://www.freepascal.org/pic/logo.gif",
-  },
-];
+// Initial State
+document.getElementById("root").innerHTML = `
+  <div class="flex flex-col h-screen w-screen bg-slate-800 text-slate-300 justify-center items-center">
+  <i class="fa-solid fa-spinner text-[64px] text-pink-400 animate-spin mb-4"></i>
+    <p>Please wait...</p>
+  </div>
+`;
 
-const dataActivities = [
-  {
-    icon: "fa-solid fa-bed",
-    title: "Sleep",
-    color: "text-orange-500",
-  },
-  {
-    icon: "fa-solid fa-dumbbell",
-    title: "Workout",
-    color: "text-teal-500",
-  },
-  {
-    icon: "fa-solid fa-utensils",
-    title: "Eat",
-    color: "text-sky-500",
-  },
-  {
-    icon: "fa-solid fa-laptop-code",
-    title: "Coding",
-    color: "text-red-500",
-  },
-  {
-    icon: "fa-solid fa-motorcycle",
-    title: "Touring",
-    color: "text-yellow-500",
-  },
-  {
-    icon: "fa-solid fa-volleyball",
-    title: "Volleyball",
-    color: "text-blue-500",
-  },
-  {
-    icon: "fa-solid fa-hands-praying",
-    title: "Pray",
-    color: "text-pink-500",
-  },
-  {
-    icon: "fa-solid fa-repeat",
-    title: "Repeat",
-    color: "text-purple-400",
-  },
-];
-
-const dataFrameworks = [
-  {
-    title: "Tailwind CSS",
-    logo: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg",
-  },
-  {
-    title: "Styled Component",
-    logo: "https://styled-components.com/icon.png",
-  },
-  {
-    title: "Ant Design",
-    logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
-  },
-  {
-    title: "React JS",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-  },
-  {
-    title: "Redux",
-    logo: "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
-  },
-  {
-    title: "Android",
-    logo: "https://developer.android.com/static/images/brand/Android_Robot_200.png",
-  },
-  {
-    title: "Flutter",
-    logo: "https://storage.googleapis.com/cms-storage-bucket/4fd5520fe28ebf839174.svg",
-  },
-  {
-    title: "BloC",
-    logo: "https://raw.githubusercontent.com/felangel/bloc/master/docs/assets/bloc_logo_full.png",
-  },
-  {
-    title: "React Native",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-  },
-  {
-    title: "Next JS",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg",
-  },
-  {
-    title: "Nest JS",
-    logo: "https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg",
-  },
-  {
-    title: "Arduino",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/8/87/Arduino_Logo.svg",
-  },
-];
-
-const dataSocMed = [
-  {
-    icon: "fa-brands fa-instagram",
-    title: "Instagram",
-    color: "text-fuchsia-500",
-    url: `href="https://instagram.com/sabituddin_bigbang" target="_blank" rel="noopener noreferrer"`,
-  },
-  {
-    icon: "fa-brands fa-facebook",
-    title: "Facebook",
-    color: "text-blue-500",
-    url: "",
-  },
-  {
-    icon: "fa-brands fa-twitter",
-    title: "Twitter",
-    color: "text-cyan-500",
-    url: "",
-  },
-  {
-    icon: "fa-brands fa-tiktok",
-    title: "Tik Tok",
-    color: "text-pink-500",
-    url: "",
-  },
-  {
-    icon: "fa-brands fa-youtube",
-    title: "Youtube",
-    color: "text-red-500",
-    url: "",
-  },
-  {
-    icon: "fa-brands fa-github",
-    title: "Github",
-    color: "text-slate-300",
-    url: `href="https://github.com/dotxbt" target="_blank" rel="noopener noreferrer"`,
-  },
-];
-
-const HeaderCard = () => {
+// HEADER COMPONENT
+const HeaderCard = async () => {
   const card = document.createElement("div");
   card.classList = `flex flex-col md:flex-row min-h-screen w-full justify-center items-center flex-1 bg-gray-800`;
   card.appendChild(ProfileCard());
-  card.appendChild(SocMedCard());
+  card.appendChild(await SocMedCard());
   return card;
 };
 
@@ -214,39 +34,25 @@ const ProfileCard = () => {
   return card;
 };
 
-const SocMedCard = () => {
+const SocMedCard = async () => {
   const card = document.createElement("div");
   const cardChild = document.createElement("div");
   card.classList = `flex flex-col justify-center items-start h-auto md:h-full p-4 md:p-8 bg-slate-700 w-full md:w-auto md:bg-transparent`;
   cardChild.classList = `max-w-[1360px] flex flex-col w-full justify-around items-start p-4 flex-1`;
-  cardChild.innerHTML = dataSocMed.map(
-    (e) => `
+
+  await fetch("./data_socmed.json")
+    .then((res) => res.json())
+    .then((data) => {
+      cardChild.innerHTML = data.map(
+        (e) => `
         <a ${e.url} class="flex flex-row cursor-pointer hover:animate-bounce items-center space-x-4  m-4 p-4 rounded-xl flex-1">
             <i class="${e.icon} text-[32px] text-center w-[46px] ${e.color}"></i>    
             <p class="text-2xl text-slate-400">${e.title}</p>
         </a>
     `
-  );
+      );
+    });
   cardChild.innerHTML = cardChild.innerHTML.replace(/,/g, "");
-  card.append(cardChild);
-  return card;
-};
-
-const FrameworkCard = () => {
-  const card = document.createElement("div");
-  const cardChild = document.createElement("div");
-  card.classList = `flex flex-col w-full justify-start items-center p-2 md:p-8 bg-slate-200`;
-  cardChild.classList = `flex flex-wrap py-4 px-4 md:px-10`;
-  cardChild.innerHTML = dataFrameworks.map(
-    (e) => `
-      <a ${e.url} class="flex flex-col cursor-pointer items-center min-w-[100px] hover:animate-bounce  m-4 p-4 rounded-xl flex-1">
-        <img class=" h-[56px] w-[56px] object-contain" src="${e.logo}" width={64} height={64}/> 
-        <p class="text-xl text-slate-600 text-center">${e.title}</p>
-      </a>
-      `
-  );
-  cardChild.innerHTML = cardChild.innerHTML.replace(/,/g, "");
-  card.innerHTML = `<div class="text-lg md:text-2xl text-slate-600 font-medium mb-4"><h1>F R A M E W O R K S   |   L I B R A R I E S</h1></div>`;
   card.append(cardChild);
   return card;
 };
@@ -255,50 +61,42 @@ const FooterCard = () => {
   const card = document.createElement("div");
   card.classList = `w-full bg-slate-800 text-center text-md text-slate-300 py-6 px-10`;
   card.innerHTML = `
-    Made with love | Sabituddin Bigbang @ ${new Date().getFullYear()}. All Right Reserved
+    Made with love | Sabituddin Bigbang @ ${new Date().getFullYear()} | All Right Reserved
   `;
   return card;
 };
 
-const ActivitiesCard = () => {
+// SECTION COMPONENT
+const SectionCard = async ({ headerTitle, data, colorBg, colorFg }) => {
   const card = document.createElement("div");
   const cardChild = document.createElement("div");
-  card.classList = `flex flex-col w-full justify-around items-center p-4 md:p-8 flex-1 bg-slate-600`;
+  card.classList = `flex flex-col w-full justify-start items-center p-0 md:p-4 ${colorBg}`;
   cardChild.classList = `flex flex-wrap py-4 px-4 md:px-10`;
-  cardChild.innerHTML = dataActivities.map(
-    (e) => `
-        <div class="flex items-center hover:animate-bounce min-w-[100px] flex-col m-4 p-4 rounded-xl flex-1">
-            <i class="${e.icon} text-[46px] text-center w-[56px] ${e.color}"></i>    
-            <p class="text-xl mt-6 text-slate-300">${e.title}</p>
-        </div>
-    `
-  );
-  cardChild.innerHTML = cardChild.innerHTML.replace(/,/g, "");
-  card.innerHTML = `<div class="text-lg md:text-2xl text-slate-200 font-medium mb-4"><h1>A C T I V I T I E S</h1></div>`;
-  card.append(cardChild);
-  return card;
-};
 
-const CodingCard = () => {
-  const card = document.createElement("div");
-  const cardChild = document.createElement("div");
-  card.classList = `flex flex-col w-full justify-start items-center p-4 md:p-8 flex-1 bg-slate-100`;
-  cardChild.classList = `flex flex-wrap py-4 px-4 md:px-10`;
-  cardChild.innerHTML = dataCoding.map(
-    (e) => `
-        <div class="flex items-center hover:animate-bounce min-w-[100px]  flex-col m-4 p-4 rounded-xl flex-1">
-            ${
-              e.url
-                ? `<img class="w-[46px] h-[46px] w-[56px] object-contain" src="${e.url}"></i>`
-                : `<i class="${e.icon} text-[46px] text-center w-[56px] ${e.color}"></i>`
-            }    
-            <p class="text-xl mt-6 text-slate-600">${e.title}</p>
-        </div>
-    `
-  );
-  cardChild.innerHTML = cardChild.innerHTML.replace(/,/g, "");
-  card.innerHTML = `<div class="text-lg md:text-2xl text-slate-600 font-medium mb-4"><h1>L A N G U A G E S | I N T E R P R E T E R S</h1></div>`;
+  await fetch(data)
+    .then((res) => res.json())
+    .then((data) => {
+      cardChild.innerHTML = data
+        ? data.map(
+            (e) => `
+      <a ${
+        e.url
+      } class="flex flex-col cursor-pointer items-center min-w-[100px] hover:animate-bounce  m-4 p-4 rounded-xl flex-1">
+        ${
+          e.logo
+            ? `<img class=" h-[56px] w-[56px] object-contain" src="${e.logo}" width={64} height={64}/>`
+            : `<i class="${e.icon} text-[46px] text-center w-[56px] h-[56px] ${e.color}"></i>`
+        }  
+        <p class="text-xl ${colorFg} mt-4 text-center">${e.title}</p>
+      </a> 
+      `
+          )
+        : "Loading....";
+      cardChild.innerHTML = cardChild.innerHTML.replace(/,/g, "");
+    });
+  card.innerHTML = `<div class="text-lg md:text-2xl tracking-widest ${colorFg} font-medium p-4 mt-4"><h1>${headerTitle}</h1></div>`;
   card.append(cardChild);
+
   return card;
 };
 
@@ -313,17 +111,38 @@ const Container = (children) => {
 };
 
 // APPS
-function App() {
+const App = async () => {
   let children = [
-    HeaderCard(),
-    ActivitiesCard(),
-    CodingCard(),
-    FrameworkCard(),
+    await HeaderCard(),
+    await SectionCard({
+      headerTitle: "ACTIVITIES",
+      data: "./data_activity.json",
+      colorBg: "bg-slate-600",
+      colorFg: "text-slate-300",
+    }),
+    await SectionCard({
+      headerTitle: "LANGUAGES | INTERPRETERS",
+      data: "./data_language.json",
+      colorBg: "bg-slate-100",
+      colorFg: "text-slate-600",
+    }),
+    await SectionCard({
+      headerTitle: "FRAMEWORKS | LIBRARIES",
+      data: "./data_framework.json",
+      colorBg: "bg-slate-200",
+      colorFg: "text-slate-600",
+    }),
     FooterCard(),
   ];
   return Container(children);
-}
+};
 
 // Document ROOT
-var root = document.getElementById("root");
-root.appendChild(App());
+const RootApp = async () => {
+  var root = document.getElementById("root");
+  const app = await App();
+  root.innerHTML = "";
+  root.appendChild(app);
+};
+
+RootApp();
