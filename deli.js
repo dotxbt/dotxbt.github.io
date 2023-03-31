61be9d4f154cd3a8521e7f12459e28b2b37a7444
Sabituddin Bigbang <sabituddin@inception.asia>
2023-04-01 01:40:08 +0700

    21

--- deli.js
+++ deli.js
@@ -12,7 +12,7 @@ const LinkCard = async () => {
   const card = document.createElement("div");
   const cardChild = document.createElement("div");
   card.classList = `flex flex-col min-h-screen w-full justify-center items-center`;
-  cardChild.classList = `flex flex-col space-y-6 w-full p-4 max-w-md mb-8`;
+  cardChild.classList = `flex flex-col space-y-6 w-full p-4 max-w-sm mb-8`;
   const logo = document.createElement("img");
   logo.classList = "h-[200px] w-[200px] object-cover my-6 shadow-xl rounded-full drop-shadow-2xl box-shadow-xl";
   logo.src = "./deli.png";
@@ -23,9 +23,9 @@ const LinkCard = async () => {
       cardChild.innerHTML = data
         ? data.map(
             (e) => `
-      <a  href="${e.url}" target="_blank" active:opacity-50 rel="noopener noreferrer" class="flex flex-row cursor-pointer w-full shadow-2xl items-center bg-white/60 rounded-xl p-4">
-        <img class=" h-[64px] w-[64px] object-contain" src="${e.logo}" width={64} height={64}/>
-        <p class="text-3xl text-slate-800 font-bold flex-1 text-center">${e.title}</p>
+      <a  href="${e.url}" target="_blank" active:opacity-50 rel="noopener noreferrer" class="flex flex-row cursor-pointer w-full shadow-2xl items-center bg-white/60 rounded-xl p-3">
+        <img class=" h-[46px] w-[46px] object-contain" src="${e.logo}" width={64} height={64}/>
+        <p class="text-2xl text-slate-800 font-bold flex-1 text-center">${e.title}</p>
       </a> 
       `
           )
