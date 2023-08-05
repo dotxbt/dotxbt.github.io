cff643280a1a6936f6aa80f9d54de0ea156e22aa
Sabituddin Bigbang <sabit@gachor.com>
2023-08-05 12:07:01 +0700

    Update Font Al-Barzanji

--- js/albarzanji.js
+++ js/albarzanji.js
@@ -1,7 +1,7 @@
 const Albarzanji = async () => {
   const parentEl = document.createElement("div");
   parentEl.classList =
-    "flex flex-col items-start w-full  max-w-4xl text-slate-800";
+    "flex flex-col items-start w-full  max-w-5xl text-slate-800";
 
   await fetch("./data/albarzanji.json")
     .then((res) => {
@@ -29,12 +29,12 @@ const SectionCard = (parentEl, title, partTitle, bismillahTitle, data) => {
   parentEl.appendChild(headerBab);
 
   const headerTitle = document.createElement("p");
-  headerTitle.classList = `text-center p-4 text-3xl w-full leading-[2.4] bg-amber-200 `;
+  headerTitle.classList = `text-center p-4 text-3xl md:text-[28pt] w-full leading-[2.0] md:leading-[2.4] bg-amber-200 `;
   headerTitle.innerHTML = partTitle;
   parentEl.appendChild(headerTitle);
 
   const bismillah = document.createElement("p");
-  bismillah.classList = `text-center p-4 text-[24pt] sm:text-[32pt] w-full leading-[1.8] bg-green-200`;
+  bismillah.classList = `text-center p-4 text-[26pt] sm:text-[42pt] w-full leading-[1.8] bg-green-200`;
   bismillah.innerHTML = bismillahTitle;
   parentEl.appendChild(bismillah);
 
@@ -46,7 +46,7 @@ const SectionCard = (parentEl, title, partTitle, bismillahTitle, data) => {
       }`;
       const dataItem = `${e}`.split("#");
       const txt = document.createElement("p");
-      txt.classList = `text-end p-4 flex-1 text-3xl w-full leading-[2.4] `;
+      txt.classList = `text-end p-4 flex-1 text-[28pt] w-full leading-[2.0] md:leading-[2.4] `;
       txt.innerHTML = dataItem[1];
       containerItem.appendChild(txt);
 
@@ -56,7 +56,7 @@ const SectionCard = (parentEl, title, partTitle, bismillahTitle, data) => {
       containerItem.appendChild(separator);
 
       const txt2 = document.createElement("p");
-      txt2.classList = `text-start p-4 flex-1 text-3xl w-full leading-[2.4] `;
+      txt2.classList = `text-start p-4 flex-1 text-[28pt] w-full leading-[2.0] md:leading-[2.4] `;
       txt2.innerHTML = dataItem[0];
       containerItem.appendChild(txt2);
       parentEl.appendChild(containerItem);
@@ -67,23 +67,23 @@ const SectionCard = (parentEl, title, partTitle, bismillahTitle, data) => {
       }`;
       const dataItemMobile = `${e}`.split("#");
       const txtMobile = document.createElement("p");
-      txtMobile.classList = `text-end p-4 flex-1 text-3xl w-full leading-[2.4] `;
+      txtMobile.classList = `text-end p-4 flex-1 text-3xl w-full leading-[2.0] md:leading-[2.4] `;
       txtMobile.innerHTML = dataItemMobile[0];
       containerItemMobile.appendChild(txtMobile);
 
       const separatorMobile = document.createElement("div");
-      separatorMobile.classList = "-my-6 flex flex-row items-center w-full";
+      separatorMobile.classList = "-my-2 flex flex-row items-center w-full";
       separatorMobile.innerHTML = `<p class="bg-slate-300 flex-1 h-[2px]"/><div><i class="fa-solid text-[14px] text-teal-600 mx-2 fa-certificate"></i></div><p class="bg-slate-300 flex-1 h-[2px]"/>`;
       containerItemMobile.appendChild(separatorMobile);
 
       const txt2Mobile = document.createElement("p");
-      txt2Mobile.classList = `text-start p-4 flex-1 text-3xl w-full leading-[2.4] `;
+      txt2Mobile.classList = `text-start p-4 flex-1 text-3xl w-full leading-[2.0] md:leading-[2.4] `;
       txt2Mobile.innerHTML = dataItemMobile[1];
       containerItemMobile.appendChild(txt2Mobile);
       parentEl.appendChild(containerItemMobile);
     } else {
       const txt = document.createElement("p");
-      txt.classList = `text-end p-4 text-3xl w-full leading-[2.4] ${
+      txt.classList = `text-end p-4 text-3xl md:text-[28pt] w-full leading-[2.0] md:leading-[2.4] ${
         i % 2 === 0 ? "bg-slate-100" : "bg-slate-200"
       }`;
       txt.innerHTML = e;
@@ -96,7 +96,7 @@ const SectionCard = (parentEl, title, partTitle, bismillahTitle, data) => {
 const Container = (children) => {
   let appContainer = document.createElement("div");
   appContainer.classList =
-    "flex flex-1 flex-col bg-slate-600 items-center line-space";
+    "flex flex-1 flex-col bg-gradient-to-br from-cyan-500 via-teal-500 to-blue-500 items-center line-space";
   for (let i = 0; i < children.length; i++) {
     appContainer.appendChild(children[i]);
   }
