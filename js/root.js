4d815aa48244a736f2d08412b98e91b66ace5332
Sabituddin Bigbang <sabituddin22@gmail.com>
2024-11-20 13:52:25 +0700

    Update CV

--- js/root.js
+++ js/root.js
@@ -27,15 +27,28 @@ const ProfileCard = async () => {
        <img class="w-[220px] h-[220px] md:w-[300px] md:h-[300px] object-cover rounded-full bg-gradient-to-tl from-pink-300 via-cyan-300 to-violet-300 p-3 md:p-4 m-10" 
         src="${data.image}"
        />
+       <img class="w-[220px] absolute h-[220px] md:w-[300px] md:h-[300px] rounded-full p-3 md:p-4 m-10" 
+        src="./assets/otw.png"
+       />
        <div class="flex md:h-[46px] md:w-[46px] h-[36px] w-[36px] absolute md:top-[240px] md:left-[288px] top-[190px] left-[218px]">
           <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-lime-400 opacity-75"></span>
           <span class="relative inline-flex rounded-full md:h-[46px] md:w-[46px] h-[36px] w-[36px] bg-green-500"></span>
         </div>
       </div>
     <div class="flex flex-col items-center pb-0 md:pb-10">
-        <h1 class="text-xl text-slate-300 font-medium">Hello, I'm <a href="https://instagram.com/sabituddin_bigbang" target="_blank" rel="noopener noreferrer" class="text-2xl font-bold text-fuchsia-200">Sabituddin Bigbang</a></h1>
+        <h1 class="text-xl text-slate-300 font-medium">Hello, I'm <a href="https://instagram.com/sabituddin_bigbang" target="_blank" rel="noopener noreferrer" class="text-2xl font-bold text-lime-500">Sabituddin Bigbang</a></h1>
         <p class="text-lg mt-2 italic text-slate-300">"Do what you love, love what you do"</p>
-    </div>`;
+    </div>
+    <h1 class="text-slate-300 mt-8 p-2">Open for Application Development positions :</h1>
+    <div class="flex flex-wrap w-full max-w-[320px] font-medium">
+      <div class="py-1 px-3 border-[1px] cursor-pointer hover:bg-green-500 hover:text-slate-800 border-green-500 rounded-xl m-2 text-green-500">Mobile</div>
+      <div class="py-1 px-3 border-[1px] cursor-pointer hover:bg-purple-500 hover:text-slate-800 border-purple-500 rounded-xl m-2 text-purple-500">Web</div>
+      <div class="py-1 px-3 border-[1px] cursor-pointer hover:bg-amber-500 hover:text-slate-800 border-amber-500 rounded-xl m-2 text-amber-600">Backend</div>
+      <div class="py-1 px-3 border-[1px] cursor-pointer hover:bg-pink-500 hover:text-slate-800 border-pink-500 rounded-xl m-2 text-pink-500">EDC Machine</div>
+      <div class="py-1 px-3 border-[1px] cursor-pointer hover:bg-sky-500 hover:text-slate-800 border-sky-500 rounded-xl m-2 text-sky-600">IoT</div>
+    </div>
+    
+    `;
     });
   cardParent.append(card);
   return cardParent;
@@ -78,7 +91,7 @@ const SectionCard = async ({ headerTitle, data, colorBg, colorFg }) => {
   const card = document.createElement("div");
   const cardChild = document.createElement("div");
   card.classList = `flex flex-col w-full justify-start items-center p-0 md:p-4 ${colorBg}`;
-  cardChild.classList = `flex grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8  py-4 gap-4 px-4 w-full`;
+  cardChild.classList = `flex grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 py-4 md:py-8 gap-4 md:gap-8 px-4 w-full`;
 
   await fetch(data)
     .then((res) => res.json())
@@ -88,13 +101,13 @@ const SectionCard = async ({ headerTitle, data, colorBg, colorFg }) => {
             (e) => `
       <a ${
         e.url
-      } class="flex bg-white rounded-xl flex-col cursor-pointer justify-start items-center min-w-[100px] hover:animate-bounce p-4 rounded-2xl">
+      } class="flex rounded-xl flex-col cursor-pointer justify-start items-center min-w-[100px] hover:animate-bounce p-4 rounded-2xl">
         ${
           e.logo
-            ? `<img class=" h-[100px] w-full px-4 flex-1 object-contain" src="${e.logo}"/>`
+            ? `<img class=" h-[100px] w-[120px] p-4 flex-1 object-contain" src="${e.logo}"/>`
             : `<i class="${e.icon} text-[80px] text-center w-full flex flex-col items-center justify-center flex-1 h-[80px] ${e.color}"></i>`
         }  
-        <p class="text-xl ${colorFg} mt-4 text-center text-slate-700">${e.title}</p>
+        <p class="text-xl ${colorFg} mt-4 text-center">${e.title}</p>
       </a> 
       `
           )
@@ -131,13 +144,13 @@ const App = async () => {
       headerTitle: "LANGUAGES | INTERPRETERS",
       data: "./data/language.json",
       colorBg: "bg-slate-100",
-      colorFg: "text-slate-600",
+      colorFg: "text-slate-800",
     }),
     await SectionCard({
       headerTitle: "FRAMEWORKS | LIBRARIES",
       data: "./data/framework.json",
       colorBg: "bg-slate-200",
-      colorFg: "text-slate-600",
+      colorFg: "text-slate-800",
     }),
     await SectionCard({
       headerTitle: "TEMPLATES",
